9c2132755fccb8f02b58f0a44ea34c50c0ed6e7b
Shakeel <shakeelkoper@gmail.com>
2024-06-15 14:16:09 +0530

    adding console log to trigger action

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MarketingApp from './components/marketingapp';
 
 export default () => {
+    console.log('Container App')
     return <div>
         <h1>Container</h1>
         <hr />
