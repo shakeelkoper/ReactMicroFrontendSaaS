98b0d0a4d7c714851e81df60ce3e201050227875
Shakeel <shakeelkoper@gmail.com>
2024-07-13 20:41:14 +0530

    Fixing Marketing App Reference

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MarketingApp from './components/marketingapp';
+import MarketingApp from './components/MarketingApp';
 
 export default () => {
     console.log('Container App')
