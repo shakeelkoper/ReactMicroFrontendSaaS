7cacee3d4de1cf174ef8565dbc567901925c6ac6
Shakeel <shakeelkoper@gmail.com>
2024-07-13 20:55:30 +0530

    Adding Public Path to Webpack Prod Config

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js', // [contenthash] is a hash of the content of the file
+        publicPath: '/container/latest/' // This is the path where the files are going to be hosted
     },
     plugins: [
         new ModuleFederationPlugin({
